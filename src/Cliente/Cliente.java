0a28695fc94d404f30af7975e76db3088dde6981
mateusaugusto <mateusaugustooliveira@gmail.com>
2015-07-15 11:18:39 -0300

    teste

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -23,7 +23,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-public class Cliente {
+public class Cliente That is the teste {
   public static int id ;
     static class Chat extends Observable {
 
